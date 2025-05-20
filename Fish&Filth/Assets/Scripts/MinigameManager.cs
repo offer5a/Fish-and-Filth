c2963f329aac1902bdf06c8a8871408ffeaa0dd0
offer5a <offbauch@gmail.com>
2025-05-20 13:53:53 +0300

    Save fast

--- Fish&Filth/Assets/Scripts/MinigameManager.cs
+++ Fish&Filth/Assets/Scripts/MinigameManager.cs
@@ -3,9 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using System.Threading;
 
 public class MinigameManager : MonoBehaviour
 {
+
+    [SerializeField] Transform TopPivot;
+    [SerializeField] Transform BottomPivot;
+    [SerializeField] Transform PlayerBar;
+
+    float BarPos;
+    float BarDes;
+
+    float BarTimer;
+    [SerializeField] float TimeMult = 3f;
+
+    float BarSpeed;
+    [SerializeField] float Speed;
+
+    float smoothMotion = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +31,16 @@ public class MinigameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        BarTimer -= Time.deltaTime;
+        if (BarTimer > 0)
+        {
+            //BarTimer = UnityEngine.Random.value * TimeMult;
+
+           // BarDes = UnityEngine.Random.value;
+        }
+
+        BarPos = Mathf.SmoothDamp(BarPos, BarDes, ref Speed, smoothMotion);
+        PlayerBar.position = Vector3.Lerp(BottomPivot.position, TopPivot.position, BarPos);
+        PlayerBar.position = Vector3.Lerp(TopPivot.position, BottomPivot.position, BarPos);
     }
 }
