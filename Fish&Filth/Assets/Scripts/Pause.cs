eb88e89dff51688336dd0cc903b2cb857e2fc22a
whitebackground <105784957+whitebackground@users.noreply.github.com>
2025-06-03 15:26:59 +0300

    Pause

---
+++ Fish&Filth/Assets/Scripts/Pause.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject PausePanel;
+
+    public void PauseButtonPressed()
+     {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0f;
+     }
+
+    public void ContinueButtonPressed()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    } 
+
+
+
+    // Update is called once per frame
+    
+}
